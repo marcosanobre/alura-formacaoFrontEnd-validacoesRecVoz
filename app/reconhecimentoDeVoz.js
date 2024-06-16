7d3eda581bb8f3cc4178bca344b1edd889d545b7
Marcos Nobre <marconobre@gmail.com>
2024-06-16 19:18:20 -0300

    Iniciando Reconhecimento de Voz

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,6 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+
